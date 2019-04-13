8a622dc645aa5dc27b1b58f7f204919f3c24e5a3
Artem <1488artemartem@gmail.com>
2019-04-13 20:14:37 +0300

    add input from keybord

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,25 @@
+import javafx.scene.transform.Scale;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Input amount worked hours: ");
+        int x = scan.nextInt();
+        System.out.println("Input your tariff");
+        int y = scan.nextInt();
 
-        System.out.println("You will get " + payment(160,250));
+
+
+        System.out.println("You will get " + payment(x,y)+" rub");
 
     }
 
 
 
-    public static  int payment (int hours, int tarif){
-        int salary = hours*tarif;
+    public static  int payment (int hours, int tariff){
+        int salary = hours*tariff;
             return salary;
     }
 
