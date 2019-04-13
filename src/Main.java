da0bc2da621a5c0e4c0d25c762ef1ec5cd2c1a7c
Artem <1488artemartem@gmail.com>
2019-04-13 19:39:36 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("You will get " + payment(160,250));
+
+    }
+
+
+
+    public static  int payment (int hours, int tarif){
+        int salary = hours*tarif;
+            return salary;
+    }
+
+
+        }
